20828573e2fa2d358d3af2c737c801bb3ea963d4
Bruno Barbosa <brunobarbosa90@hotmail.com>
2024-08-22 17:59:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace condicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade = 34;
+
+            if (idade > 18)
+            {
+               Console.WriteLine("Verdade");
+            }
+            else{
+               Console.WriteLine("Mentira"); 
+            }
+
+            
+            
+
+
+            Console.ReadLine();
+              
+        }
+    }
+}
